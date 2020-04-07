e6a364479ff00ee91cefce7cc92cd3bf424dabc1
mmurgolo <matteomurgolo@gmail.com>
2020-04-07 16:55:14 +0200

    push

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let x=0;
 let y=1;
+
 function clicker() {
   x = x + y;
 document.getElementById("demo").innerHTML = x;
+refreshOptions()
 }
 
+function refreshOptions()
+{
+    if (x < 25)
+        document.getElementById(“ovenBtn”).disabled = true;
+    else
+        document.getElementById(“ovenBtn”).disabled = false;
+}
 
 function achat() {
 if (x >= 25) {
@@ -12,4 +21,5 @@ if (x >= 25) {
   y = y + 1;
 }
 document.getElementById("demo").innerHTML = x;
+refreshOptions()
 }
